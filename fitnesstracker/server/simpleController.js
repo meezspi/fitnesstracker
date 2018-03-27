2c5f7786de25ef34d08c4104450a5669828b30a9
Premiere Piano <miaspiri@gmail.com>
2018-03-27 13:58:34 -0400

    revised updates

---
+++ fitnesstracker/server/simpleController.js
@@ -0,0 +1,17 @@
+var express = require('express')
+var app = express.Router();
+
+
+module.exports = app
+.use(function(req,res,next){
+res.write('This is provided by Mia Spiri at newpaltz.edu\r\n');
+next();
+})
+.get('/hello', function (req, res) {
+res.write('World');
+res.end();
+})
+.get('/goodbye', function (req, res) {
+res.write('New Paltz');
+res.end();
+})
--- fitnesstracker/src/app/app.module.ts
+++ fitnesstracker/src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import {RouterModule,Routes} from '@angular/router';
 import { AppComponent } from './app.component';
+import { MessagesComponent } from './messages/messages.component';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+import { DataComponent } from './data/data.component';
+
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MessagesComponent,
+    HomeComponent,
+    NavComponent,
+    DataComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'home',component: HomeComponent },
+      {path: 'data', component: DataComponent},
+      { path: '', redirectTo: 'home',pathMatch:'full'}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
